578e73d69c3a79fb2a512b8a18c514ca6b13d195
secondl1f3 <ahmad.fajar@outlook.co.id>
2021-05-19 18:20:53 +0700

    rename the variable to make easier to read

--- src/main/java/MazeGenerator.java
+++ src/main/java/MazeGenerator.java
@@ -1,3 +1,7 @@
+/**
+ * @author ahmadfajar - ahmad.fajar@outlook.co.id
+ * @since 19/05/2021
+ */
 public class MazeGenerator {
 
     /**
@@ -6,17 +10,16 @@ public class MazeGenerator {
      * @param size - int
      */
     public static void generateMaze(int size) {
-        int lastIndexFromSize = size * 2;
         int initialSize = 0;
-        for (int i = 1; i <= size; i++) {
-            if (i % 2 == 1) {
+        for (int verticalWall = 1; verticalWall <= size; verticalWall++) {
+            if (verticalWall % 2 == 1) {
                 initialSize++;
             }
-            for (int j = 1; j <= size; j++) {
-                if (j == 1 || j == size) {
+            for (int horizontalWall = 1; horizontalWall <= size; horizontalWall++) {
+                if (horizontalWall == 1 || horizontalWall == size) {
                     System.out.print("@");
-                } else if (i % 2 == 1) {
-                    if ((j == 2 && initialSize % 2 == 1) || (j == (size - 1) && initialSize % 2 == 0)) {
+                } else if (verticalWall % 2 == 1) {
+                    if ((horizontalWall == 2 && initialSize % 2 == 1) || (horizontalWall == (size - 1) && initialSize % 2 == 0)) {
                         System.out.print(" ");
                     } else {
                         System.out.print("@");
